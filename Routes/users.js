06aeba3a4d867044dd31d8cad26c515640f1530f
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2022-06-05 21:02:51 +0500

    Fixing Fetch User Protected Route

--- Routes/users.js
+++ Routes/users.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { signIn, signUp } from "../Controller/auth.js";
 import { getUsers } from "../Controller/users.js";
+import { auth } from "../Middleware/auth.js";
+import admin from "../Middleware/admin.js";
 import { isEmailValid } from "../Middleware/auth.js";
 //router
 const router = express.Router();
@@ -8,7 +10,7 @@ const router = express.Router();
 //routes
 router.post("/signin", signIn);
 router.post("/signup", signUp);
-router.get("/", getUsers);
+router.get("/", auth, admin, getUsers);
 // router.patch("/:id", updateUser);
 // router.delete("/:id", deleteUser);
 
