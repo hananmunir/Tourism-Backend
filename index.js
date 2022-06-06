aea81b112f271e622a48f584a1145da60252704c
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2022-06-06 12:11:10 +0500

    Pushin final changed

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const app = express();
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 app.use("/api/packages/", packageRoutes);
